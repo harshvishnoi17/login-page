4a3676a1f07d9efcbe324eb7cc9018b4a3087cb2
Bhupesh Singh Padiyar <bhupesh.singh@codapayments.com>
2024-06-29 20:16:34 +0800

    Designed home page profile

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { LogoutComponent } from '../logout/logout.component';
+import { StorageService } from '../_services/storage.service';
 
 
 @Component({
@@ -14,9 +15,16 @@ import { LogoutComponent } from '../logout/logout.component';
 })
 export class HomeComponent  {
 
-  constructor(private router: Router) { }
+  user: any;
+
+  constructor(private router: Router,
+    private storageService: StorageService
+  ) { }
 
   ngOnInit(): void {
+
+    this.user = this.storageService.getUser()
+    console.log(this.user);
   }
 
   goBack(): void {
